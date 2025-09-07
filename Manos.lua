333cc64f0344e50d7b8e50cba4440d39f9529141
Ajolote-cmyk <omarmarron.knotion@gmail.com>
2025-09-06 19:56:20 -0700

    Update Manos.lua

--- Manos.lua
+++ Manos.lua
@@ -1,3 +1,8 @@
 while true do
- if game.Workspace.tono.Reflectance = 0 then 
-      game.Workspace.chilaquiles.nahual.PointLight.Enable = false
+    if game.Workspace.tono.Reflectance == 0 then
+        game.Workspace.chilaquiles.nahual.PointLight.Enabled = false
+    else
+        game.Workspace.chilaquiles.nahual.PointLight.Enabled = true
+    end
+    wait(0.1)  -- ← Evita que Roblox se crashee
+end
