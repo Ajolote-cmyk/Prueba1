45e4738ba7d50c46bff844dafbe7ea9c78c2b6cb
Ajolote-cmyk <omarmarron.knotion@gmail.com>
2025-09-06 19:16:03 -0700

    Update Manos.lua

--- Manos.lua
+++ Manos.lua
@@ -1,3 +1,6 @@
 while true do
-  game.Workspace.Rig.Head.Reflectance = 0
+ wait (10)
+ if game.Workspace.Rig.Head.Reflectance = 0 then 
+      game.Workspace.chilaquiles.nahual.PointLight.Enable = false
+  
 end
